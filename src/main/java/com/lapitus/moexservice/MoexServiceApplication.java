4daa9454f9a339c2195e7780df2074f4e7bc3a61
Alexey Gaponchuk <lapitusik@gmail.com>
2022-04-25 20:45:58 +0300

    first commit

---
+++ src/main/java/com/lapitus/moexservice/MoexServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lapitus.moexservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoexServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoexServiceApplication.class, args);
+    }
+
+}
